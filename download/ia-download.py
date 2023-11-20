db09f3e8806c83c86bc89655da09576d6aabcf0d
Derek Quenneville <techknight@techknight.com>
2023-11-20 17:32:01 -0500

    Add utility: ia-download.py

---
+++ download/ia-download.py
@@ -0,0 +1,87 @@
+# ia-download.py
+# https://github.com/techknight/llm-scripts
+#
+# This script takes an Internet Archive URL and optionally a filetype as input,
+# and does a multi-threaded download of the matching files in that folder.
+#
+# Usage:
+#   ia-download.py <URL> [filetype]
+# 
+#   <URL> must be a /download folder on archive.org, ie https://archive.org/download/Computer_Chronicles/Season%2003/
+#   [filetype] is optional, defaults to zip
+#
+# Example: (downloads all MP4 files from Season 03 of the Computer Chronicles)
+#   ia-download.py https://archive.org/download/Computer_Chronicles/Season%2003/ mp4
+#
+# Prerequisites:
+#   BeautifulSoup
+#   pip install beautifulsoup4
+#   
+#   TQDM
+#   pip install tqdm
+#
+# Tool: 
+#   ChatGPT (GPT-4)
+
+import os
+import requests
+from bs4 import BeautifulSoup
+from concurrent.futures import ThreadPoolExecutor
+from tqdm import tqdm
+import sys
+
+# Get the URL from the command-line arguments
+if len(sys.argv) < 2:
+    print('Please specify the URL of the archive.org folder.')
+    sys.exit()
+url = sys.argv[1]
+
+# Get the file type from the command-line arguments or default to .zip
+file_type = '.zip'
+if len(sys.argv) >= 3:
+    file_type = '.' + sys.argv[2] if not sys.argv[2].startswith('.') else sys.argv[2]
+
+# Request the HTML content of the page
+response = requests.get(url)
+
+# Parse the HTML content using BeautifulSoup
+soup = BeautifulSoup(response.content, 'html.parser')
+
+# Find all links that end with the specified file type and do not have "View Contents" in their text
+file_links = soup.find_all('a', href=lambda href: href is not None and href.endswith(file_type) and 'View Contents' not in href)
+
+# Create a directory to store the downloaded files
+download_dir = f'downloaded_{file_type[1:]}s'  # e.g., downloaded_zips or downloaded_pdfs
+if not os.path.exists(download_dir):
+    os.makedirs(download_dir)
+
+# Define a function to download a single file with progress bar
+def download_file(file_url):
+    filename = file_url.split('/')[-1]
+    filepath = os.path.join(download_dir, filename)
+    response = requests.get(file_url, stream=True)
+    with open(filepath, 'wb') as f:
+        total_size = int(response.headers.get('content-length', 0))
+        block_size = 1024
+        progress_bar = tqdm(total=total_size, unit='iB', unit_scale=True, desc=filename)
+        try:
+            for data in response.iter_content(block_size):
+                if tqdm._instances:
+                    progress_bar.update(len(data))
+                    f.write(data)
+                else:
+                    # Progress bar has been closed by user
+                    break
+        except KeyboardInterrupt:
+            # User pressed the q key to quit
+            progress_bar.close()
+            print('\nDownload interrupted.')
+            sys.exit()
+
+# Use multithreading to download the files
+with ThreadPoolExecutor(max_workers=10) as executor:
+    for link in file_links:
+        file_url = url + link.get('href')
+        executor.submit(download_file, file_url)
+
+print(f'All {file_type[1:]} files downloaded.')
